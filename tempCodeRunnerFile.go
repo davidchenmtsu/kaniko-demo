acfff2a01ecc9c85d3b73ec04cfe408dd4a8d548
David Chen <davidchen@Davids-MacBook-Pro.local>
2023-11-28 11:59:39 -0700

    v3

---
+++ tempCodeRunnerFile.go
@@ -0,0 +1,16 @@
+// hello.go
+
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func main() {
+    fmt.Printf(
+        "Hello world from %s/%s\n",
+        runtime.GOOS,
+        runtime.GOARCH,
+    )
+}
